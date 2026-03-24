680882dce34a49a17e03d1da91d80bf6c4dfece2
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-24 15:42:06 +0100

    complete milestone 3: refactor Products component to filter products based on budget mode

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Product from "../components/Product";
 import Loader from "../components/Loader";
+import BudgetContext from "../context/BudgetContext";
 
 export default function Products() {
   const [products, setProducts] = useState(null);
+  const { budgetMode } = useContext(BudgetContext);
 
   function getProducts() {
     axios.get("https://fakestoreapi.com/products").then((res) => {
@@ -13,6 +15,15 @@ export default function Products() {
   }
 
   useEffect(getProducts, []);
+
+  const visibleProducts = products ?
+    products.filter((product) =>{
+      if(!budgetMode){
+          return true
+      }
+      return product.price <= 30
+  })
+  : [];
 
   return (
     <main>
@@ -27,16 +38,15 @@ export default function Products() {
       </section>
 
       <section>
-        {products === null && <Loader/>}
+        {products === null && <Loader />}
         {products !== null && (
-
-        <div className="container my-5">
-          <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4">
-            {products.map((product) => (
-              <Product product={product} key={product.id}></Product>
-            ))}
+          <div className="container my-5">
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4">
+              {visibleProducts.map((product) => (
+                <Product product={product} key={product.id}></Product>
+              ))}
+            </div>
           </div>
-        </div>
         )}
       </section>
     </main>
